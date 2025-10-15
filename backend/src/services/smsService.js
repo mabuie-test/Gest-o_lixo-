1a3174e3cf246027aeba6773408a062d85df9908
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-15 18:46:52 +0200

    Add smsService for sending SMS messages
    Created a service for sending SMS messages.

---
+++ backend/src/services/smsService.js
@@ -0,0 +1,7 @@
+// placeholder: integrate Twilio / or control gateway that triggers SMS via device modem
+module.exports = {
+  async sendSms(to, message){
+    console.log('SMS ->', to, message);
+    // implement provider here
+  }
+};
