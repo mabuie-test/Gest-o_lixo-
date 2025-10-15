4f857489b76b57d36e895d82a8bfa0fcea6dbb0c
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-15 20:16:33 +0200

    Fix import statement formatting in AdminPanel.jsx

--- frontend/src/components/AdminPanel.jsx
+++ frontend/src/components/AdminPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+ import React, { useEffect, useState } from 'react';
 import { fetchDevices } from '../api';
 
 export default function AdminPanel({ user }){
