e5d9e4339823036a84f81c7ce217273582b86cee
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-15 20:04:06 +0200

    Update devices.js

--- backend/src/routes/devices.js
+++ backend/src/routes/devices.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const router = express.Router();
 const Device = require('../models/Device');
 const auth = require('../middleware/authMiddleware');
