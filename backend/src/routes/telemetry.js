28855dc5473a3e6f081ddd09224032e39643d720
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-15 20:05:34 +0200

    Fix formatting of express import statement

--- backend/src/routes/telemetry.js
+++ backend/src/routes/telemetry.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const router = express.Router();
 const Telemetry = require('../models/Telemetry');
 const Device = require('../models/Device');
